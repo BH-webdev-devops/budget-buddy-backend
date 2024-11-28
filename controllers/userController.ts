c7c176d432c2aa802a12c12781e28fa2fc44dbbb
buzasg <george.buzas@bahag.com>
2024-11-28 14:14:48 +0100

    /users only for logged admins

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -3,8 +3,9 @@ import {query} from '../database/db'
 
 export const getUserProfile = async (req: Request, res: Response): Promise<Response | any> => {
     const userId = (req as Request & { user: any }).user.id
+    console.log(userId)
     try {
-        const result = await query(`SELECT id, username, email FROM users WHERE id = $1`, [userId])
+        const result = await query(`SELECT * FROM users WHERE id = $1`, [userId])
         const user = result.rows[0]
 
         if(!user){
@@ -21,11 +22,15 @@ export const getUserProfile = async (req: Request, res: Response): Promise<Respo
 }
 
 export const getUsers = async (req: Request, res: Response): Promise<Response | any> => {
-    const userAdmin = (req as Request & { user: any }).user.is_admin
-    if (userAdmin === false) {
-        return res.status(403).json({message: "Forbidden access"})
+    
+    // Check if the current user that is logged in is an admin or not
+    const userId = (req as Request & { user: any }).user.id;
+    const userJson = await query(`SELECT is_admin FROM users WHERE id = $1`, [userId]);
+    const isAdmin = userJson.rows[0].is_admin;
+    if (!isAdmin) {
+        return res.status(403).json({ message: `You don't have permission to view this data` })
     }
-    try {
+    try {   
         const result = await query(`SELECT * FROM users`)
         const users = result.rows
 
