816e9e5db06d344eefa21ed46789089c891da2f3
ozyegenl <lara.ozyegen@bahag.com>
2024-11-28 14:19:46 +0100

    spending controller

---
+++ controllers/spendingController.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from 'express'
+import {query} from '../database/db'
+
+export const createSpending = async (req: Request, res: Response): Promise<Response | any> => {
+    const userId = (req as Request & { user: any }).user.id
+    const { name, amount, date, category } = req.body
+    try {
+        
+        const result = await query(`INSERT INTO spendings (name, amount, date, category, user_id) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [name, amount, date, category, userId])
+        if (!result) {
+            return res.status(404).json({ message: `Spending not created` })
+        }
+        return res.status(201).json({
+            message: `Spending created successfully`, task: result.rows[0]
+        })
+    }
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: `Internal server error` })
+    }
+}
---
+++ types/Spending.ts
@@ -0,0 +1,10 @@
+interface Spending {
+    id : number;
+    name : string;
+    amount : number;
+    date : string;
+    category: string;
+    user_id: number;
+}
+
+export type {Spending}
