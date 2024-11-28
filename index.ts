322b1957dd6ba8d6d36fed85f972d85fe5b316f1
buzasg <george.buzas@bahag.com>
2024-11-28 12:02:51 +0100

    /profile works

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors'; 
 import { pool } from './database/db';
 import authRouter from './routers/authRouter';
+import userRouter from './routers/userRouter';
 
 const PORT = process.env.PORT;
 const app = express(); // create express app
@@ -16,7 +17,7 @@ const startServer = async () => {
         const client = await pool.connect();
         console.log(`Connection with the database established 🟢`)
         client.release();
-        app.use('/api', authRouter);
+        app.use('/api', authRouter, userRouter);
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
--- routers/userRouter.ts
+++ routers/userRouter.ts
@@ -1,5 +1,9 @@
 import {Router} from 'express'
+import { authenticateJWT } from '../middlewares/jwtMiddleware'
+import {getUserProfile} from '../controllers/userController';
 
 const userRouter : Router = Router()
 
-//userRouter.get('/users', checkAdmin, getAllUsers);
+userRouter.get('/profile', authenticateJWT, getUserProfile);
+
+export default userRouter
