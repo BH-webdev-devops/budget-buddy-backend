9d119f237af6a0f456855663138ae8f1a21a5b0e
buzasg <george.buzas@bahag.com>
2024-12-02 15:59:41 +0100

    display error in the response

--- controllers/spendingController.ts
+++ controllers/spendingController.ts
@@ -46,7 +46,7 @@ export const createSpending = async (req: Request, res: Response): Promise<Respo
 
     catch (err) {
         console.log(err)
-        return res.status(500).json({ message: `Internal server error` })
+        return res.status(500).json({ message: `Internal server error`, err })
     }
 }
 
